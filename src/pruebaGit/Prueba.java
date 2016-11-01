5cdc9feb889b6c1bc5e435b07eac4ef16a1b3fda
pacogomezarnal <paco.gomez.arnal@gmail.com>
2016-11-01 19:16:17 +0100

    kk5

--- src/pruebaGit/Prueba.java
+++ src/pruebaGit/Prueba.java
@@ -4,7 +4,8 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("KK4");
+		System.out.println("KK5"
+				+ "");
 	}
 
 }
